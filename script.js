c92185c4a695f7c74ab3ab545b4999ab0139749f
iguessicanexplainthings <shaanlourem946@gmail.com>
2025-05-02 13:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-    question.innerHTML =  "Finally tumne haan bol hi dia🤭🫶🏻
-        ILOVEYOUSOMUCH";
+    question.innerHTML =  "Finally tumne haan bol hi dia🤭🫶🏻";
   gif.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHBpZmdlMnVtZjRyNDhjOWlkcnBleWV3MXVsYWtjY3NuOW5vbm11OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fa2oJItOZwEiQ/giphy.gif";
 });
 
