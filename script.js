d3725aac4e079433bdfac19312078d62710c13ae
iguessicanexplainthings <shaanlourem946@gmail.com>
2025-05-02 13:28:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-    question.innerHTML =  "I LOVE HER VERY MUCH MORE THAN ANYTHING I HAVE RIGHT NOW I WANT HER TO BE THE MOST HAPPIEST PERSON IN MY LIFE";
+    question.innerHTML =  "Finally tumne haan bol hi dia🤭🫶🏻
+        ILOVEYOUSOMUCH";
   gif.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHBpZmdlMnVtZjRyNDhjOWlkcnBleWV3MXVsYWtjY3NuOW5vbm11OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fa2oJItOZwEiQ/giphy.gif";
 });
 
